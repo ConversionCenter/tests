4b8f4a8c07618d48d68a8e34542185ff0e40b8f6
ConversionCenter <doodzyskania1krok@proton.me>
2025-06-26 11:40:07 +0200

    Create interfaceForRemix.sol

---
+++ interfaceForRemix.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IConversionCenterEndUser {
+    struct DecentralizedPassport {
+        string nameP;
+        string nameC;
+        string Sirname;
+        bytes32 country;
+        bytes32 decentralizedID;
+    }
+ 
+    function createProfileConvertAndVoteForDemocracy(
+        string memory nameP,
+        string memory nameC,
+        string memory Sirname,
+        bool ifIRAN,
+        bytes32 inputCountry,
+        uint256 indexOfAnswer,
+        bytes32 decentralizedID) external payable;
+    function viewCitizen(uint256 index) external view returns (DecentralizedPassport memory);
+
+    function spuff() external view returns (uint256);
+    function indexStatement() external view returns (uint256);
+    function statements(uint256) external view returns (string memory);
+    function declarations(bytes32, uint256) external view returns (uint256);
+    function howManyVoted(bytes32) external view returns (uint256);
+    function citizens(uint256) external view returns (
+        string memory nameP,
+        string memory nameC,
+        string memory Sirname,
+        bytes32 country,
+        bytes32 decentralizedID
+    );
+    function citizensAdr(address) external view returns (uint256);
+    function citizenAdresFromIndex(uint256) external view returns (address);
+    function indexCitizens() external view returns (uint256);
+    function decentralizedIDAdres(address) external view returns (bytes32);
+    function adresOfdecentralizedID(bytes32) external view returns (address);
+    function view11Rules() external view returns (string memory);
+
+
+
+
+}
